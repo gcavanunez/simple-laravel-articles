b87554c077f199aa7f2a644fbe0a03e39d9c66c5
Guillermo <gcavanunez@gmail.com>
2019-08-21 22:58:00 -0500

    added the seeders

--- database/seeds/ArticlesTableSeeder.php
+++ database/seeds/ArticlesTableSeeder.php
@@ -1,5 +1,9 @@
 <?php
 
+
+use App\Article;
+
+use App\User;
 use Illuminate\Database\Seeder;
 
 class ArticlesTableSeeder extends Seeder
@@ -11,6 +15,17 @@ class ArticlesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+
+        $faker = \Faker\Factory::create();
+
+        User::all()->each(function ($user) use ($faker) {
+            foreach (range(1, 5) as $i) {
+                Article::create([
+                    'user_id' => $user->id,
+                    'title'   => $faker->sentence,
+                    'content' => $faker->paragraphs(3, true),
+                ]);
+            }
+        });
     }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+
+use App\User;
 use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
@@ -11,6 +13,22 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+
+        $faker = \Faker\Factory::create();
+        $password = bcrypt('secret');
+
+        User::create([
+            'name'     => $faker->name,
+            'email'    => 'master@domain.com',
+            'password' => $password,
+        ]);
+
+        for ($i = 0; $i < 10; ++$i) {
+            User::create([
+                'name'     => $faker->name,
+                'email'    => $faker->email,
+                'password' => $password,
+            ]);
+        }
     }
 }
